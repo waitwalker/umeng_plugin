f7e76db4d9ddd31bfd434894b5e2052d79cbe689
waitwalker <waitwalker@163.com>
2020-07-02 08:57:18 +0800

    iOS端插件封装

--- ios/Classes/UmengPlugin.m
+++ ios/Classes/UmengPlugin.m
@@ -1,4 +1,6 @@
 #import "UmengPlugin.h"
+#import <UMMobClick/MobClick.h>
+#import <UMMobClick/MobClickSocialAnalytics.h>
 
 @implementation UmengPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -12,9 +14,53 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if ([@"init" isEqualToString:call.method]) {
+    [self init:call result:result];
+  } else if ([@"logPageView" isEqualToString:call.method]) {
+    [MobClick logPageView:call.arguments[@"name"] seconds:[call.arguments[@"seconds"] intValue]];
+    result(nil);
+  } else if ([@"beginPageView" isEqualToString:call.method]) {
+    [MobClick beginLogPageView:call.arguments[@"name"]];
+    result(nil);
+  } else if ([@"endPageView" isEqualToString:call.method]) {
+    [MobClick endLogPageView:call.arguments[@"name"]];
+    result(nil);
+  } else if ([@"logEvent" isEqualToString:call.method]) {
+    if (call.arguments[@"label"] != [NSNull null])
+      [MobClick event:call.arguments[@"name"] label:call.arguments[@"label"]];
+    else
+      [MobClick event:call.arguments[@"name"]];
+    result(nil);
+  } else if ([@"reportError" isEqualToString:call.method]) {
+    result(nil);
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void)init:(FlutterMethodCall*)call result:(FlutterResult)result {
+  UMConfigInstance.appKey = call.arguments[@"key"];
+  // UMConfigInstance.secret = call.arguments[@"secret"];
+
+  NSString* channel = call.arguments[@"channel"];
+  if (channel) UMConfigInstance.channelId = channel;
+
+  NSNumber* policy = call.arguments[@"policy"];
+  if (policy) UMConfigInstance.eSType = [policy intValue];
+
+  NSNumber* reportCrash = call.arguments[@"reportCrash"];
+  if (reportCrash) UMConfigInstance.bCrashReportEnabled = [reportCrash boolValue];
+
+  [MobClick startWithConfigure:UMConfigInstance];
+
+  NSNumber* logEnable = call.arguments[@"logEnable"];
+  if (logEnable) [MobClick setLogEnabled:[logEnable boolValue]];
+
+  NSNumber* encrypt = call.arguments[@"encrypt"];
+  if (encrypt) [MobClick setEncryptEnabled:[encrypt boolValue]];
+
+  NSNumber* interval = call.arguments[@"interval"];
+  if (interval) [MobClick setLogSendInterval:[interval doubleValue]];
 }
 
 @end
